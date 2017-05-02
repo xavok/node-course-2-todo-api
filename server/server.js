e8b6727aceb899adf470405142ddde711fea5eb2
Xavok <xavoking@gmail.com>
2017-05-02 08:51:57 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -8,11 +8,12 @@ const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {ObjectID}= require('mongodb');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/todos', (req,res) => {
+app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
@@ -23,12 +24,25 @@ app.post('/todos', (req,res) => {
     });
 });
 
-app.get('/todos', (req,res) => {
-   Todo.find().then((todos) => {
-       res.send({todos})
-   }, (e) => {
-       res.status(400).send(e);
-   });
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch(() => res.status(400).send());
 });
 
 app.listen(3000, () => {
