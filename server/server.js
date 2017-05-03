c198d622071be4425ec4072ecf22ecda63194f6b
Xavok <xavoking@gmail.com>
2017-05-03 00:11:20 -0700

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@
 "use strict";
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -54,6 +55,29 @@ app.delete('/todos/:id', (req,res) => {
         return res.status(404).send();
     }
     Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch(() => res.status(400).send());
+});
+
+app.patch('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new:true}).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
