ee6a6dfec4e0a5bdadd073998fb59c866424ea5e
Xavok <xavoking@gmail.com>
2017-05-03 22:13:50 -0700

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -6,11 +6,12 @@ const config = require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
-const {ObjectID}= require('mongodb');
+const {authenticate} = require('./middleware/authenticate');
 
 const app = express();
 const port = process.env.PORT;
@@ -50,7 +51,7 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
-app.delete('/todos/:id', (req,res) => {
+app.delete('/todos/:id', (req, res) => {
     var id = req.params.id;
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
@@ -63,7 +64,7 @@ app.delete('/todos/:id', (req,res) => {
     }).catch(() => res.status(400).send());
 });
 
-app.patch('/todos/:id', (req,res) => {
+app.patch('/todos/:id', (req, res) => {
     var id = req.params.id;
     var body = _.pick(req.body, ['text', 'completed']);
 
@@ -71,14 +72,14 @@ app.patch('/todos/:id', (req,res) => {
         return res.status(404).send();
     }
 
-    if(_.isBoolean(body.completed) && body.completed) {
+    if (_.isBoolean(body.completed) && body.completed) {
         body.completedAt = new Date().getTime();
     } else {
         body.completed = false;
         body.completedAt = null;
     }
 
-    Todo.findByIdAndUpdate(id, {$set: body}, {new:true}).then((todo) => {
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
@@ -86,16 +87,21 @@ app.patch('/todos/:id', (req,res) => {
     }).catch(() => res.status(400).send());
 });
 
-app.post('/users', (req,res) => {
+app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
     user.save().then(() => {
         return user.generateAuthToken();
     }).then((token) => {
-        res.header('x-auth',token).send(user);
+        res.header('x-auth', token).send(user);
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
